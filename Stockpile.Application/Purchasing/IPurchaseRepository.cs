8300c58c225eefa9d28f9711f61cd8bdbf5e5276
Joseph Wilson <ioe416@gmail.com>
2026-09-02 10:26:18 -0400

    Add IPurchaseRepository and IReceiptRepository interfaces
    Introduced IPurchaseRepository for async Purchase retrieval by ID in Stockpile.Application.Purchasing. Added IReceiptRepository in Stockpile.Application.Purchasing.Receiving for async Receipt operations (get, add, update). Removed "Purchasing\Receiving\" Folder entry from project file.

---
+++ Stockpile.Application/Purchasing/IPurchaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using Stockpile.Domain.Purchasing.Models;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace Stockpile.Application.Purchasing
+{
+    public interface IPurchaseRepository
+    {
+        Task<Purchase?> GetByIdAsync(
+        Guid purchaseId,
+        CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Stockpile.Application/Purchasing/Receiving/IReceiptRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using Stockpile.Domain.Purchasing.Models;
+
+namespace Stockpile.Application.Purchasing.Receiving;
+
+public interface IReceiptRepository
+{
+    Task <Receipt?> GetByIdAsync(
+        Guid accountId,
+        CancellationToken cancellationToken = default);
+
+    Task AddAsync(
+        Receipt receipt,
+        CancellationToken cancellationToken = default);
+
+    Task UpdateAsync(
+        Receipt receipt,
+        CancellationToken cancellationToken = default);
+}
